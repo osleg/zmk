e7fd11984b453ad20c0577076d535fe959293177
Nick Conway <nick@conway.dev>
2022-05-23 16:33:08 -0400

    Add configurable sensor bindings

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -33,6 +33,7 @@ module.exports = {
       "behaviors/tap-dance",
       "behaviors/caps-word",
       "behaviors/key-repeat",
+      "behaviors/sensor-rotate",
       "behaviors/reset",
       "behaviors/bluetooth",
       "behaviors/outputs",
